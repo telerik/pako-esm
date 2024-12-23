a15a19ec90d2f8d0e1beda27877104c0c5bd152e
Tsvetomir Tsonev <tsvetomir.tsonev@progress.com>
2024-12-23 12:26:19 +0200

    chore: update rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import buble from '@rollup/plugin-buble';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default [{
     // ESM2015 and SystemJS bundles
